bb1d8f52c60f8793f3252cea97f181ae12a97816
devbcdestiller <dev.bcdestiller@gmail.com>
2024-07-22 22:41:47 +0800

    initial commit

---
+++ src/vite.config.ts
@@ -0,0 +1,29 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+    strictPort: true
+  }
+})
